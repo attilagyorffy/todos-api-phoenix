8e3985d608257b63d105ccbf704885ecf804f305
Attila Györffy <attila@attilagyorffy.com>
2016-07-23 22:49:33 +0300

    Use JaSerializer.PhoenixView in TodoView

--- web/views/todo_view.ex
+++ web/views/todo_view.ex
@@ -1,17 +1,6 @@
 defmodule PhoenixTodosApi.TodoView do
   use PhoenixTodosApi.Web, :view
+  use JaSerializer.PhoenixView
 
-  def render("index.json", %{todos: todos}) do
-    %{data: render_many(todos, PhoenixTodosApi.TodoView, "todo.json")}
-  end
-
-  def render("show.json", %{todo: todo}) do
-    %{data: render_one(todo, PhoenixTodosApi.TodoView, "todo.json")}
-  end
-
-  def render("todo.json", %{todo: todo}) do
-    %{id: todo.id,
-      title: todo.title,
-      is_completed: todo.is_completed}
-  end
+  attributes [:title, :is_completed]
 end
