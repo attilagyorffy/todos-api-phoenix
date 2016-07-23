903625fcf9b62631b13b9cd7b1c2d26566476e69
Attila Györffy <attila@attilagyorffy.com>
2016-07-23 21:10:01 +0300

    Seed the db with some sample data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixTodosApi.Repo
+alias PhoenixTodosApi.Todo
+
+Repo.insert!(%Todo{title: "Create the Phoenix App", is_completed: true})
+Repo.insert!(%Todo{title: "Prepare the Ember App", is_completed: false})
+Repo.insert!(%Todo{title: "Ensure the Apps Work Together", is_completed: false})
