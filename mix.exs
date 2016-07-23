abcfa8b3a21b4ed9ed78de1b99de1357454f3a54
Attila Györffy <attila@attilagyorffy.com>
2016-07-23 22:14:56 +0300

    Add ja_serializer to Phoenix

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule PhoenixTodosApi.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:corsica, "~> 0.5"}]
+     {:corsica, "~> 0.5"},
+     {:ja_serializer, "~> 0.10"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
